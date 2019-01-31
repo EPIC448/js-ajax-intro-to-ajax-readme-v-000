36789e62a456431198530e47b2b3f8f7adddae47
epic0448 <samuelogundiran@Yahoo.com>
2019-01-31 16:07:38 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,14 @@
+
+function showRepositories() {
+//this is set to the XMLHttpRequest object that fired the event
+var repos = JSON.parse(this.responseText);   
+console.log(repos);
+    const repoList = `<ul>${repos.map}(r => '<li>' + r.name '" onclick="getCommit(this)"'+ '</li>').join('')} </ul>`;
+        
+    document.getElementById('repositories').innerHTML = repoList;
+}
+function getRepositories() {
+    const req = new XMLHttpRequest();
+    req.open('GET','https://api.github.com/users/octocat/repos');
+    req.send();
+}
